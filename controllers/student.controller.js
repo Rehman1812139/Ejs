1610541b658da7d17e795181769b0a143632d661
Abdul Rehman <74251165+Rehman1812139@users.noreply.github.com>
2021-04-08 20:45:46 +0500

    Add files via upload

---
+++ controllers/student.controller.js
@@ -0,0 +1,52 @@
+const Student= require('../models/student.model')
+exports.test=(req,res)=>{
+    res.send('Testing Controller')
+}
+
+exports.student_create=(req,res)=>{
+let student=new Student({
+    Name:req.body.Name,
+    Gender:req.body.Gender,
+    City:req.body.City
+})
+student.save((err)=>{
+if(err){
+    return next (err)
+}
+res.send('Student has been added')
+})
+}
+
+exports.student_details=(req,res)=>{
+    Student.findById(req.params.id,(err,student)=>{
+        res.send(student)
+    })
+}
+
+exports.student_update=(req,res)=>{
+    Student.findByIdAndUpdate(req.params.id, {$set: req.body}, (err,student)=>{
+        if(err) return next(err)
+        res.send('Students list has been updated')
+    })
+
+}
+
+exports.student_delete=(req,res)=>{
+Student.findByIdAndRemove(req.params.id, (err)=>{
+      if (err) return next(err)
+      res.send('Deleted Successfully.')
+
+})
+
+}
+
+exports.list=(req,res)=>{
+    Student.find((err,students)=>{
+        if(!err){
+            res.render('student',{page:'Student List', menuId:'list',students:students})
+        }
+        else{
+        console.log('Cannot retrieve students'+JSON.stringify(err,underfined,2));
+        }
+    })
+}
